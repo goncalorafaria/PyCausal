cd411b806f9d8c41231dd3fab214b0f887b7b971
Gonçalo Faria <gonca2372@gmail.com>
2021-04-29 11:31:25 +0100

    adding multivar problemas

--- pycausal/problems.py
+++ pycausal/problems.py
@@ -1,5 +1,6 @@
 from .scm import *
 from .distributions import norm
+import random
 
 def linear_norm(name):
 
@@ -12,7 +13,7 @@ def linear_norm(name):
     return X
 
 def pack(model, opaque, X, Y, Z, adj_matrix):
-    pars = [ (k,v) for k,v in model.nodes.items() if "A" == k[0] or "B" == k[0] ] 
+    pars = [ (k,v) for k,v in model.nodes.items() if "A" == k[0] or "B" == k[0] ]
 
     given = { v: float((~v)(1)) for k,v in pars }
 
@@ -28,7 +29,7 @@ def NormalCollider(opaque=True):
     Ayz = HiddenVariable("Ayz", norm(loc=3,scale=4))
 
     X = linear_norm("x") << "X"
-    
+
     Y = linear_norm("y") << "Y"
 
     Z = Axz*X + Ayz*Y + linear_norm("z") << "Z"
@@ -44,7 +45,7 @@ def NormalChain(opaque=True):
     Axy = HiddenVariable("Axy", norm(loc=5,scale=4))
 
     X = linear_norm("x") << "X"
-    
+
     Y = Axy*X + linear_norm("y") << "Y"
 
     Z = Ayz*Y + linear_norm("z") << "Z"
@@ -60,7 +61,7 @@ def NormalFork(opaque=True):
     Axy = HiddenVariable("Axy", norm(loc=0,scale=4))
 
     X = linear_norm("x") << "X"
-    
+
     Y = Axy*X + linear_norm("y") << "Y"
 
     Z = Axz*X + linear_norm("z") << "Z"
@@ -73,11 +74,11 @@ def NormalMediator(opaque=True):
     model = SCM("Simple Normal Fork")
 
     Axz = HiddenVariable("Axz", norm(loc=1,scale=4))
-    Axy = HiddenVariable("Axy", norm(loc=3,scale=4))
+    Axy = HiddenVariable("Axy", norm(loc=3,scale=6))
     Ayz = HiddenVariable("Ayz", norm(loc=0,scale=4))
 
     X = linear_norm("x") << "X"
-    
+
     Y = Axy*(X) + linear_norm("y") << "Y"
 
     Z = Ayz*(Y) + Axz*(X) + linear_norm("z") << "Z"
@@ -87,4 +88,71 @@ def NormalMediator(opaque=True):
     return pack(model, opaque, X, Y, Z, adj_matrix)
 
 
-    
+def pack_listing(model, var_list, adj_matrix):
+    pars = [ (k,v) for k,v in model.nodes.items() if "A" == k[0] or "B" == k[0] ]
+    given = { v: float((~v)(1)) for k,v in pars }
+
+    return model&given, var_list, adj_matrix
+
+
+def RandomLinearNormal(n=3, max_degree=3):
+    model = SCM("Simple Random Network")
+
+    adj_matrix = np.zeros((n,n),dtype=np.int)
+    k = 0
+    frontier=[]
+    while k < n :
+        Nxi = linear_norm("x"+str(k))
+        input_nodes = random.sample(
+            list(range(k)),
+            min(
+                random.randint(0, max_degree),
+                k)
+            )
+
+        nc = Nxi
+
+        for i in input_nodes:
+            adj_matrix[i,k]=1
+            rv = frontier[i]
+            scale = HiddenVariable("A"+Nxi.name+":"+rv.name, norm(loc=3,scale=6))
+            nc += scale * rv
+
+        nc << "X"+str(k)
+        frontier.append(nc)
+
+        k+=1
+
+    return pack_listing(model, frontier, adj_matrix)
+
+
+def RandomFourierNormal(n=3, max_degree=3):
+    model = SCM("Simple Fourier Random Network")
+
+    adj_matrix = np.zeros((n,n),dtype=np.int)
+    k = 0
+    frontier=[]
+    while k < n :
+        Nxi = linear_norm("x"+str(k))
+        input_nodes = random.sample(
+            list(range(k)),
+            min(
+                random.randint(0, max_degree),
+                k)
+            )
+
+        nc = Nxi
+
+        for i in input_nodes:
+            adj_matrix[i,k]=1
+            rv = frontier[i]
+            scale = HiddenVariable("A"+Nxi.name+":"+rv.name, norm(loc=3,scale=6))
+            phi = HiddenVariable("B"+Nxi.name+":"+rv.name, norm(loc=0,scale=2))
+            nc += scale * sin( phi * rv )
+
+        nc << "X"+str(k)
+        frontier.append(nc)
+
+        k+=1
+
+    return pack_listing(model, frontier, adj_matrix)
